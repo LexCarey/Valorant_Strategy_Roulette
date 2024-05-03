517b59de291ba814108187263d11768b035690ad
lexCarey <lexcarey73@gmail.com>
2024-05-02 22:45:55 -0700

    Map Select CSS and Menu

--- client/src/components/Map_Select/Map_Select.jsx
+++ client/src/components/Map_Select/Map_Select.jsx
@@ -5,19 +5,43 @@ import axios from 'axios'
 
 const Map_Select = (props) => {
     const {maps} = props
+    const [mapMenu, setmapMenu] = useState(false)
+    const [selectedmap, setSelectedmap] = useState(false)
+
+    let mapMenuTrue = () => {
+        setmapMenu(!mapMenu)
+    }
+
+    let mapSelect = (map = []) => {
+        setmapMenu(!mapMenu)
+        setSelectedmap(map)
+    }
 
     return (
         <div style={{width: "100%"}}>
-            
-            {
+
+            <div className='map-choice' onClick={mapMenuTrue}>
+                {
+                    selectedmap ?
+                    <div className='mapContainer' style={{color: "white", fontFamily: "valorant", width: "100%"}}>
+                        <img className='map-image-cover' style={{width: "100%"}} src={selectedmap.splash} alt={selectedmap.displayName} />
+                        <h2 className='mapText'>{selectedmap.displayName}</h2>
+                    </div>
+                    :
+                    <h1 className='plus'>+</h1>
+                }
+            </div>
+            { mapMenu &&
+            <div className='mini-maps'>
+                {
                 maps.map((entry, i) =>
-                    <div className='mapContainer' key={i} style={{color: "white", fontFamily: "valorant", width: "100%"}}>
-                        <img style={{width: "100%"}} src={entry.listViewIcon} alt={entry.displayName} />
+                    <div className='mapContainer one-map' key={i} style={{color: "white", fontFamily: "valorant", width: "100%"}} onClick={(e) => mapSelect(entry)}>
+                        <img style={{width: "100%"}} src={entry.splash} alt={entry.displayName} />
                         <h1 className='mapText'>{entry.displayName}</h1>
-                        <br/>
-                        <br/>
                     </div>
                 )
+                }
+            </div>
             }
         </div>
     )
